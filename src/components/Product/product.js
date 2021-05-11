4983713dc367467952e066e7bac305853d6f732b
yashrajgure <yashrajgure0808@gmail.com>
2021-05-11 12:37:24 +0530

    changes to product

--- src/components/Product/product.js
+++ src/components/Product/product.js
@@ -1,9 +1,17 @@
 import './product.scss'
+import { ReactVideo } from "reactjs-media"
 
 const Product=()=>{
     return(
         <div id="product">
-            <p>Product</p>
+            <div className="product-flex product-1">
+                <h2 className="heading">What is Quokkonect?</h2>
+                <div className="product-video">
+                    <video controls loop autoPlay>
+                        <source type='video/mp4' src="../../assets/quokkonnect-video.mp4" />
+                    </video>
+                </div>
+            </div>
         </div>
     )
 }
