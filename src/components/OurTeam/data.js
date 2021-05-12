d1acae3eb7f8989179cfee1b86e007167425063c
yashrajgure <yashrajgure0808@gmail.com>
2021-05-12 20:44:17 +0530

    our team final

---
+++ src/components/OurTeam/data.js
@@ -0,0 +1,74 @@
+import Shvetha from '../../assets/shvetha.png'
+import Yash from '../../assets/yash.jpg'
+import Sam from '../../assets/Sam.jpg'
+
+const data = [
+    {
+        id: 1,
+        name: "Shvetha Nambiar",
+        country:"India",
+        image: Shvetha,
+        links: {
+            linkedin: "https://www.linkedin.com/in/shvetha-nambiar-46074b1a5/",
+            instagram: "https://www.instagram.com/shvethaaaa/",
+            email: "mailto:shvethanambiar@gmail.com"
+        },
+    },
+    {
+        id: 2,
+        name: "Yash Rajgure",
+        country:"India",
+        image: Yash,
+        links: {
+            linkedin: "https://www.linkedin.com/in/yash-rajgure-b695371b0/",
+            instagram: "https://www.instagram.com/yash_rajgure/",
+            email: "mailto:yashrajgure0808@gmail.com"
+        },
+    },
+    {
+        id: 3,
+        name: "Sam Yiheng",
+        country:"Germany",
+        image: Sam,
+        links: {
+            linkedin: "https://www.linkedin.com/in/cenyiheng/",
+            instagram: "https://www.instagram.com/samyiheng/",
+            email: "mailto:hi@samyiheng.com"
+        },
+    },
+    {
+        id: 4,
+        name: "Jadie Luo",
+        country:"Hong Kong",
+        image: Shvetha,
+        links: {
+            linkedin: "",
+            instagram: "",
+            email: ""
+        },
+    },
+    {
+        id: 5,
+        name: "Anita Floredi",
+        country:"Italy",
+        image: Shvetha,
+        links: {
+            linkedin: "",
+            instagram: "",
+            email: ""
+        },
+    },
+    {
+        id: 6,
+        name: "Kartika Fissendri",
+        country:"Japan",
+        image: Shvetha,
+        links: {
+            linkedin: "",
+            instagram: "",
+            email: ""
+        },
+    }
+]
+
+export default data;
--- src/components/OurTeam/ourTeam.js
+++ src/components/OurTeam/ourTeam.js
@@ -1,9 +1,9 @@
 import './ourTeam.scss'
-import Shvetha from '../../assets/shvetha.png'
-import Yash from '../../assets/yash.jpg'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faAt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import data from './data.js'
 
 
 const TeamCard=({member})=>{
@@ -34,76 +34,6 @@ const TeamCard=({member})=>{
 }
 
 const OurTeam=()=>{
-
-    const data = [
-        {
-            id: 1,
-            name: "Shvetha Nambiar",
-            country:"India",
-            image: Shvetha,
-            links: {
-                linkedin: "https://www.linkedin.com/in/shvetha-nambiar-46074b1a5/",
-                instagram: "https://www.instagram.com/shvethaaaa/",
-                email: "mailto:shvethanambiar@gmail.com"
-            },
-        },
-        {
-            id: 2,
-            name: "Yash Rajgure",
-            country:"India",
-            image: Yash,
-            links: {
-                linkedin: "https://www.linkedin.com/in/yash-rajgure-b695371b0/",
-                instagram: "https://www.instagram.com/yash_rajgure/",
-                email: "mailto:yashrajgure0808@gmail.com"
-            },
-        },
-        {
-            id: 3,
-            name: "Sam Yiheng",
-            country:"Germany",
-            image: Shvetha,
-            links: {
-                linkedin: "",
-                instagram: "",
-                email: ""
-            },
-        },
-        {
-            id: 4,
-            name: "Jadie Luo",
-            country:"Hong Kong",
-            image: Shvetha,
-            links: {
-                linkedin: "",
-                instagram: "",
-                email: ""
-            },
-        },
-        {
-            id: 5,
-            name: "Anita Floredi",
-            country:"Italy",
-            image: Shvetha,
-            links: {
-                linkedin: "",
-                instagram: "",
-                email: ""
-            },
-        },
-        {
-            id: 6,
-            name: "Kartika Fissendri",
-            country:"Japan",
-            image: Shvetha,
-            links: {
-                linkedin: "",
-                instagram: "",
-                email: ""
-            },
-        }
-    ]
-
     return(
         <div id="ourteam">
             <h2 className="heading" >Our Team</h2>
