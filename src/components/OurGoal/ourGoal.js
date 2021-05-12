53e7530fbc912287d4a26720ab08de24ec14aae9
ShvethaNambiar <shvetha2003@gmail.com>
2021-05-12 20:41:32 +0530

    rotating fan

--- src/components/OurGoal/ourGoal.js
+++ src/components/OurGoal/ourGoal.js
@@ -1,7 +1,7 @@
 import './ourGoal.scss'
-import Goal1 from '../../assets/goal-1.svg'
+// import Goal1 from '../../assets/goal-1.svg'
 import Goal2 from '../../assets/goal-2.svg'
-
+import Goal1 from './svgRotate'
 const OurGoal = () => {
     return (
         <div id="goal">
@@ -10,7 +10,8 @@ const OurGoal = () => {
                     <h2 className="heading">Our Goal</h2>
                     <p>We aspire to connect young students from 10 to 15 years of age from different regions and walks of life. Our main goal is to bring them to appreciate the cultural diversity and understand the importance of sustainable development, while making fun, creative projects!</p>
                 </div>
-                <div className="goal-img"><img className="svg-img" src={Goal1}></img></div>
+                {/* <div className="goal-img"><img className="svg-img" src={Goal1}></img></div> */}
+                <div className=" "><Goal1 /></div>
             </div>
             <div className="goal-flex goal-2">
                 <div className="goal-text">
