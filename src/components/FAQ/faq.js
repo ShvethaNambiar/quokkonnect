e79db9d9cf52c3e3e769dd35e54ed132fdf46073
ShvethaNambiar <shvetha2003@gmail.com>
2021-05-12 13:55:43 +0530

    faq pt 4

--- src/components/FAQ/faq.js
+++ src/components/FAQ/faq.js
@@ -1,31 +1,41 @@
 import './faq.scss'
+import React, { useState } from 'react'
 import FaqData from './faqData'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import { faAngellist, faLine } from '@fortawesome/free-brands-svg-icons'
+
+const Question = ({ question, answer }) => {
+    const [expanded, setExpanded] = useState(false);
+
+    return (
+        <div className='question'>
+            <div className={!expanded ? "question-box" : "extended-questionbox"}>
+                <p className="faqquestion" onClick={() => setExpanded(!expanded)} >
+                    {question}
+                </p>
+                <button className='btn' onClick={() => setExpanded(!expanded)}>
+                    {expanded ? <FontAwesomeIcon icon={faAngellist} /> : <FontAwesomeIcon icon={faLine} />}
+                </button>
+            </div>
+            <div>
+                {expanded && <p className="faqanswer">{answer}</p>}
+            </div>
+
+        </div>
+    )
+}
 const Faq = () => {
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
-    function handleClick(e) {
-        e.preventDefault();
-        e.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    };
+    const [questions, setQuestions] = useState(FaqData)
 
     return (
         <div id="faq">
-            {/* {FaqData.map((data) => (
-                <div>
-                    <button onClick={handleClick(data.id)} className="accordion" >{data.question}</button>
-                    <div className="panel">
-                        <p >{data.answer}</p>
-                    </div>
-                </div>
-            ))} */}
+            <h2 className="heading">FAQ's</h2>
+            <div >
+                {questions.map((question) => (
+                    <Question key={question.id}{...question} />
+                ))}
+            </div>
         </div>
     );
 };
