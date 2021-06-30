e899e55d29da6deb58e126cf4ff21063c6426589
ShvethaNambiar <shvetha2003@gmail.com>
2021-06-30 12:45:15 +0530

    faqdata

--- src/components/FAQ/faqData.js
+++ src/components/FAQ/faqData.js
@@ -2,26 +2,26 @@ const FaqData = [
     {
         id: 1,
         question: "Why is this platform for the students aged 10 to 15?",
-        answer: "Kids under 10 should be freed from too much formal learning contents. Kids above 15 will be having other learning targets for senior school or even college/universities afterwards. 10 to 15 is a typical age for the formation of one’s world view. If we succeed in helping pre-teens and teens in this age group to gain awareness about sustainable development, they will carry the mindset to their later life phases."
+        answer: "10 to 15 is a typical age for the formation of one’s world view. If we succeed in helping pre-teens and teens in this age group to gain awareness about sustainable development, they will carry the mindset to their later life phases."
     },
     {
         id: 2,
         question: "Why project based learning (PBL)?",
-        answer: "Different from the traditional classroom teaching, participants in project based learning stay in the frontend. That means, they will define the project goals to solve the given challenges. They will work on their own paces to gain knowledge needed. Teachers will be turned to mentors, staying on the side to offer necessary support. Project based learning, if applied properly, can help to improve the abilities of communication, collaboration, critical thinking and self-learning."
+        answer: "Project based learning is different from the traditional classroom teaching, students stay in the frontend, that means they will define the project goals to solve the given challenges. They will work on their own paces to gain knowledge needed. Teachers will be turned to mentors, staying on the side to offer necessary support. Project based learning, can help to improve the abilities of communication, collaboration, critical thinking and self-learning."
     },
     {
         id: 3,
         question: "Are the projects you give under extracurricular or intra curricular activities for students?",
-        answer: "It depends. If the local schools are open to integrate our projects on the platform and recognize the learning effort of the students, intra curricular projects are possible.        Otherwise, our projects will be offered as extra curricular courses. Quokkonnect does not want to add extra burden to its participants."
+        answer: "Currently our projects will be offered as extra curricular courses. Quokkonnect does not want to add extra burden to its participants. In the future, we plan on integrating our platform with local schools and recognize the learning effort of the students as intra curricular projects."
     },
     {
         id: 4,
         question: "Who are the coaches/mentors?",
-        answer: "Here in Quokkonnect, we will try to engage volunteers as far as possible. We plan to provide them basic training so they will be able to efficiently mentor the students. These volunteers could be parents, school teachers, future senior graduates from Quokkonnect or even any adult willing to help make positive change in the world"
+        answer: "Here in Quokkonnect, we engage volunteers as mentors. We plan to provide them basic training so they will be able to efficiently mentor the students. These volunteers could be parents, school teachers, future senior graduates from Quokkonnect or even any adult willing to help make positive change in the world"
     }, {
         id: 5,
         question: "What are the roles of parents?",
-        answer: "Parents will be invited to participate in the events of project demonstration. Our mentors will contact parents to ensure transparency during the progress. What is more, engaged parents will be invited to join as volunteers and grow together."
+        answer: "Parents will be invited to participate in the events of project demonstration. Our mentors will contact parents to ensure transparency during the progress. Further on, engaged parents will be invited to join as volunteers and grow together."
     }, {
         id: 6,
         question: "Are all the programs online?",
@@ -29,16 +29,16 @@ const FaqData = [
     }, {
         id: 7,
         question: "What about the language barriers amongst students?",
-        answer: "Participants are totally free in choosing their most comfortable language. We plan to team up the participants based on the language preferences. English being the most global language, will offer you a chance to team up with students from across regions. However if you want to team up with students from your region you can choose a regional language."
+        answer: "Participants are totally free in choosing their most comfortable language. We would team up the participants based on the language preferences. English being the most global language, will offer you a chance to team up with students from across regions. However if you want to team up with students from your region you can choose a regional language."
     }, {
         id: 8,
         question: "How can our platform ensure Internet safety?",
-        answer: "We will offer virtual meet-ups to each team during the project phase, so that students can work on the projects in a controlled and safe-for-surfing internet environment. They can hold virtual meetings in small groups and upload their research data and materials in our secured cloud. Our IT-Team will ensure the infrastructure is safe for our participants.Apart from that, our mentors will make sure no cyber bullying takes place during the online meet-ups."
+        answer: "We offer virtual meet-ups to each team during the project phase, so that students can work on the projects in a controlled and safe-for-surfing internet environment. They can hold virtual meetings in small groups and upload their research data and materials in our secured cloud. Our IT-Team will ensure the infrastructure is safe for our participants.Apart from that, our mentors will make sure no cyber bullying takes place during the online meet-ups."
     },
     {
         id: 9,
         question: "What about costs?",
-        answer: "We charge no costs for our participants. We plan to raise funds to cover our general and administrative efforts. We will engage volunteers as far as possible to work for the platform."
+        answer: "We charge no costs for our participants! We plan to raise funds to cover our general and administrative efforts. We engage volunteers as far as possible to work for the platform."
     }
 ]
 
